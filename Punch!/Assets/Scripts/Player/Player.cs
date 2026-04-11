813192373d2de38e3ea4d7def98b0730b134683e
Alexander Avteniev <aja2328@rit.edu>
2026-04-11 14:44:44 -0400

    Restart on line walk

--- Punch!/Assets/Scripts/Player/Player.cs
+++ Punch!/Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -17,15 +18,26 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _punchJumpSpeed;
     private Vector3 _velocity;
+
     private Vector3 _forwardVector;
-    private float yRotation;
-    private float _verticalVelocity;
+    [SerializeField]
+    private LineRenderer _punchLine;
+    [SerializeField]
+    private float _maxLineLength;
+    [SerializeField]
+    private int _numPunchesPerLine;
+    private float _timeInJump;
+
+    private Vector3 _verticalVelocity;
+    private Vector3 _horizontalVelocity;
 
     [SerializeField]
     private float _bounceTimeDelay;
     private float _timer;
     private bool _isGrounded;
+    private bool _punchingStarted;
     private bool _punched;
+    private int _timesPunched;
 
     private Vector2 _lastMoveInput;
 
@@ -37,7 +49,9 @@ public class Player : MonoBehaviour
         _timer = _bounceTimeDelay;
         _isGrounded = true;
         _punched = false;
-        _verticalVelocity = 0;
+        _verticalVelocity.y = 0;
+        _punchLine.enabled = false;
+        _punchingStarted = false;
     }
     private void FixedUpdate()
     {
@@ -50,8 +64,20 @@ public class Player : MonoBehaviour
         Move();
         Gravity();
 
-        _velocity.y += _verticalVelocity * Time.deltaTime;
+        if (_punched)
+        {
+            _timeInJump += Time.fixedDeltaTime;
+        }
+
+        _velocity += _verticalVelocity * Time.fixedDeltaTime;
+
+        if(_horizontalVelocity.x > 0 || _horizontalVelocity.z > 0)
+        {
+            _velocity += _horizontalVelocity * Time.fixedDeltaTime;
+        }
+
         this.transform.position = _velocity;
+
         this.transform.forward = _forwardVector;
 
 
@@ -59,37 +85,79 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
+        /*
         Vector3 pos = this.transform.position;
         pos.x += _lastMoveInput.x * _moveSpeed * Time.fixedDeltaTime;
         pos.z += _lastMoveInput.y * _moveSpeed * Time.fixedDeltaTime;
         _velocity.x = pos.x;
         _velocity.z = pos.z;
-        if (_lastMoveInput.magnitude > 0)
+        */
+        if (_lastMoveInput.magnitude > 0 && !_punchingStarted)
         {
             _forwardVector.x = _lastMoveInput.x;
             _forwardVector.z = _lastMoveInput.y;
             _forwardVector.y = 0;
+
+            _punchLine.enabled = true;
         }
+        else if (_lastMoveInput.magnitude == 0 &&  !_punchingStarted) 
+        {
+            _punchLine.enabled = false;
+        }
+        _punchLine.SetPosition(1, transform.position + _forwardVector * _maxLineLength);
+
+        float distancePerJump = _maxLineLength / _numPunchesPerLine;
+        Vector3 nextPoint = transform.position + _forwardVector * distancePerJump;
+        if (_punched)
+        {
+            // _velocity = this.transform.position;
+            _horizontalVelocity.x += nextPoint.x*Time.fixedDeltaTime;
+            _horizontalVelocity.z += nextPoint.z*Time.fixedDeltaTime;
+        }
+        else
+        {
+            _horizontalVelocity = Vector3.zero;
+        }
+
 
     }
 
     private void Punch()
     {
-        Debug.Log(_verticalVelocity);
-        if (!_punched)
+        float distancePerJump = _maxLineLength / _numPunchesPerLine;
+
+        if (_timesPunched == 0)
         {
-            _verticalVelocity = 0;
-            _verticalVelocity += _punchJumpSpeed;
-            _punched = true;
-            _puncher.OnPunch();
+            _punchingStarted = true;
+
         }
+        _timesPunched++;
+        if (_timesPunched > _numPunchesPerLine)
+        {
+            _punchingStarted = false;
+        }
+        if (_punchingStarted)
+        {
+            if (_punchLine.enabled)
+            {
+                if (!_punched)
+                {
+                    _verticalVelocity = Vector3.zero;
+                    _verticalVelocity.y += _punchJumpSpeed;
+                    _punched = true;
+                    _puncher.OnPunch();
+                }
+            }
+        }
+
+
     }
 
     private void Jump() 
     {
         if (_isGrounded && _timer < 0)
         {
-             _verticalVelocity = _baseJumpSpeed;
+             _verticalVelocity.y = _baseJumpSpeed;
             _isGrounded = false;
         }
     }
@@ -98,14 +166,15 @@ public class Player : MonoBehaviour
     {
         if (!_isGrounded) 
         {
-            _verticalVelocity -= _gravity;
+            _verticalVelocity.y -= _gravity;
             if (_velocity.y < -.2)
             {
                 _velocity.y = 0;
                 _timer = _bounceTimeDelay;
                 _isGrounded = true;
-                _verticalVelocity = 0;
+                _verticalVelocity = Vector3.zero;
                 _punched = false;
+                _timeInJump = 0;
             }
         }
     }
